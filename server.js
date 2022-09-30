b23a1f95ef78e14e34065b1bc31eddc53ce3b2ae
OlehKulyk2710 <92692410+OlehKulyk2710@users.noreply.github.com>
2022-10-01 00:38:02 +0300

    add .env

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
-const app = require("./app");
 const mongoose = require("mongoose");
-require("dotenv").config();
+const app = require("./app");
+
+const { PORT = 3000, DB_HOST } = process.env;
 
 mongoose
-  .connect(process.env.DB_HOST)
-  .then(() => console.log("Database connection successful"))
-  .catch((error) => console.log(error.message));
+  .connect(DB_HOST)
+  .then(() => {
+    console.log("Database connection successful");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000");
-});
+    app.listen(PORT, () => {
+      console.log("Server running. Use our API on port: 3000");
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
