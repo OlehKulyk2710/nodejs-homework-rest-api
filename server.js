f6f6d67b0b40e166cbbdcbda826907d9cf08ddc9
OlehKulyk2710 <92692410+OlehKulyk2710@users.noreply.github.com>
2022-09-30 21:45:20 +0300

    db init

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
 const app = require("./app");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+mongoose
+  .connect(process.env.DB_HOST)
+  .then(() => console.log("Database connection successful"))
+  .catch((error) => console.log(error.message));
 
 app.listen(3000, () => {
   console.log("Server running. Use our API on port: 3000");
